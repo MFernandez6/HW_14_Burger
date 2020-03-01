0d99fa6722dbd37bc6c752dc6f0748e46b5b4cd6
Miguel Fernandez <miguelfernandez023@gmail.com>
2020-03-01 14:17:53 -0500

    should run

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
 var app = express();
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -14,9 +13,9 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers");
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("server listening on: https://localHost:" + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
